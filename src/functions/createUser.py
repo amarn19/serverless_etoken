c07624baf2198361fbad7cf8845ad5307b6183c4
amar-at-882180297397 <amarn19@gmail.com>
2020-07-22 10:53:39 -0500

    createUser api update

--- src/functions/createUser.py
+++ src/functions/createUser.py
@@ -9,18 +9,30 @@ def createUser(event,context,dynamodb=None):
     try:
         if not dynamodb:
                 dynamodb = boto3.resource('dynamodb')
+        
         etoken_table = dynamodb.Table(os.environ['ETOKEN_TABLE'])
         body = ast.literal_eval(event['body'])
         user_id = body['user_id']
         item_type= body['item_type']
         user_type= body['user_type']
-        print(user_id,user_type)
-        response = etoken_table.put_item(
-            Item={
+        dashboard=body['dashboard']
+        item={
                 'pk': user_id,
                 'sk': user_type,
+                'dashboard':dashboard,
                 'type': item_type
-            },
+        }
+        if user_type == "shopper":
+            user_details=body['user_details']
+            history=body['history']
+            item.update({'user_details':user_details,'history':history})
+        elif user_type == "store_owner":
+            store_details=body['store_details']
+            messages=body['messages']
+            item.update({'store_details':store_details,'messages':messages})
+        print(user_id,user_type)
+        response = etoken_table.put_item(
+            Item=item,
             ConditionExpression='attribute_not_exists(pk) AND attribute_not_exists(sk)'
         )
         print(response)
